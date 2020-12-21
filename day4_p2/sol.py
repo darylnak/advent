b7191c4143fb60be73625a062a4eb736106bac53
darylnak <darylnak@no-reply.github.com>
2020-12-20 20:45:49 -1000

    finish day4 p2

--- day4_p2/sol.py
+++ day4_p2/sol.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
         elems = {x[:x.index(':')]:x[x.index(':')+1:] for x in t if x != '' and x[:x.index(':')] != "cid"}
         if len(elems) == len(valid):
             if (1920 <= int(elems['byr']) <= 2002 and 2010 <= int(elems['iyr']) <= 2020 and 2020 <= int(elems['eyr']) <= 2030 and \
-                re.search(r'^#[0-9a-f]{6}$', elems['hcl']) and elems['ecl'] in val_ecl and re.match('^\d{9}$', elems['pid'])):
+                re.search(r'^#[0-9-a-f]{6}$', elems['hcl']) and elems['ecl'] in val_ecl and re.match('^\d{9}$', elems['pid'])):
                 if (elems['hgt'][-2:] == 'cm' and 150 <= int(elems['hgt'][:-2]) <= 193) or (elems['hgt'][-2:] == 'in' and 59 <= int(elems['hgt'][:-2]) <= 76):
                     num_valid += 1
     
