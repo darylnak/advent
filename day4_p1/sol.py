293d6d103320ed7f0fb745dc70107fec0ba478b5
darylnak <darylnak@no-reply.github.com>
2020-12-20 17:27:51 -1000

    finsh day4 p1

---
+++ day4_p1/sol.py
@@ -0,0 +1,20 @@
+# https://adventofcode.com/2020/day/4
+import re
+
+if __name__ == "__main__":
+    curr = []
+    num_valid = 0
+    valid = ["ecl","pid","eyr","byr","iyr","hgt","hcl"]
+    with open('./input.txt', 'r') as f:
+        lines = f.read().split("\n\n")
+
+    for line in lines:
+        line = re.sub(r'\n', r' ', line)
+        t = line.split(" ")
+
+        # check x != '' since the last line in the file is a single '\n'
+        curr = [x[:x.index(':')] for x in t if x != '' and x[:x.index(':')] != "cid"]
+        if len(curr) == len(valid):
+            num_valid += 1
+    
+    print("Number of valid passports: {}".format(num_valid))
